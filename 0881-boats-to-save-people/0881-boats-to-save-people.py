2d9765cded34d7da3fb7d646a33e02208bd265aa
Krina Pardiwala <krinapardiwala03@gmail.com>
2026-07-05 12:44:37 +0530

    Time: 71 ms (51.36%), Space: 16.4 MB (50.53%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        boat=0
+        people.sort()
+        left=0
+        right=len(people)-1
+
+        while left<=right:
+            if people[left]+people[right]<=limit:
+                left+=1
+            right-=1
+            boat+=1
+            
+        return boat
+
+
+        
+        
